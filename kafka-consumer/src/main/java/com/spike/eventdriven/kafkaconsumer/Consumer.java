f9b23be71598e265e7fb991c377119a33e8e3012
Firman Hanafi <firman.hanafi@doku.com>
2020-09-04 14:35:15 +0700

    Fix consumer

--- kafka-consumer/src/main/java/com/spike/eventdriven/kafkaconsumer/Consumer.java
+++ kafka-consumer/src/main/java/com/spike/eventdriven/kafkaconsumer/Consumer.java
@@ -1,10 +1,12 @@
 package com.spike.eventdriven.kafkaconsumer;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
 
+@Service
 public class Consumer {
 
-    @KafkaListener(id = "kafka", topics = "pesan-saya")
+    @KafkaListener(groupId = "kafka", topics = "pesan-saya")
     public void kafkakonsumer(String in) {
         System.out.println("Data yang ditarik : " + in);
     }
